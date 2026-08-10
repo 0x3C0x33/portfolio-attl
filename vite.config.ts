7f3e0f09624c1912404acf55ced76404704500b4
0x3C0x33 <adr793@gmail.com>
2026-08-10 15:43:57 +0200

    fix(build): disable cssMinify in build config to support xp.css selectors

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Desactivamos la minificación estricta del CSS para evitar que falle con la sintaxis de XP.css
+    cssMinify: false,
+  },
 })
